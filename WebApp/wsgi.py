9e2acfde4611177d6f5155415b3fac999720d437
cerfs21 <olivier.baxas@gmail.com>
2024-02-29 20:36:27 +0100

    Remove code and data files as development of the Web App. moved to another repository.

--- WebApp/wsgi.py
+++
@@ -1,7 +0,0 @@
-# wsgi v1.1:
-#   Changed application path in /var/www/
-
-# Bridging Python web application to Apache server using Web Server Gateway Interface
-import sys
-sys.path.insert(0,"/var/www/red-wire/")
-from red_wire_app import server as application
